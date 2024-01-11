7149d86ac9450b7c52fc4d74ee1d898ef7ea9522
vdWes911 <stephanwest911@gmail.com>
2024-01-11 18:38:26 +0200

    Added initial files

---
+++ InverterPoller/include/config.h
@@ -0,0 +1,18 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// #define WIFI_SSID "LordOfThePings"
+// #define WIFI_PW "WiFiPa$sword911!"
+
+#define EXAMPLE_ESP_WIFI_SSID "GvwHome_EXT2"
+#define EXAMPLE_ESP_WIFI_PASS "9a8b7c6d5e"
+#define EXAMPLE_ESP_WIFI_CHANNEL 1
+#define EXAMPLE_MAX_STA_CONN 4
+
+// #define MQTT_SERVER "enter_your_MQTT_SERVER"
+// #define MQTT_PORT 1883
+
+// #define MQTT_USER "enter_your_MQTT_user"
+// #define MQTT_PASS "enter_your_MQTT_password"
+
+#endif
---
+++ InverterPoller/include/main.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_mac.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+
+#include "lwip/err.h"
+#include "lwip/sys.h"
+
+#define LOG_TAG "Main"
